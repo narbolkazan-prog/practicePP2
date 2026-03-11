ebaf6254717a2e265beb8e7a65fc7f14742b68b8
Narbol Kazan <narbolkazan@gmail.com>
2026-03-11 22:07:02 +0600

    Add Practice6 - file handling and built-in functions examples

---
+++ practice05/receipt_parser/import shutil.py
@@ -0,0 +1,40 @@
+import shutil
+
+
+shutil.move("file1.txt", "folder1/file1.txt")
+
+
+
+
+shutil.move("file2.txt", "folder2/file2.txt")
+
+
+
+
+shutil.move("file3.txt", "folder3/file3.txt")
+
+
+
+
+shutil.move("file4.txt", "folder4/file4.txt")
+
+
+
+
+shutil.move("file5.txt", "folder5/file5.txt")
+
+
+
+
+
+shutil.move("c.txt", "data4/c.txt")
+
+
+
+
+shutil.move("d.txt", "folder1/d.txt")
+
+
+
+
+shutil.move("k.txt", "example/k.txt")
